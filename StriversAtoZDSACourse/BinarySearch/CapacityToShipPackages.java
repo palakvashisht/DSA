a06021476373dc09e2bda3327adce99cb3971e5d
palakbatra <palaktg27@gmail.com>
2025-07-14 14:23:10 +0530

    Ship

--- StriversAtoZDSACourse/BinarySearch/CapacityToShipPackages.java
+++ StriversAtoZDSACourse/BinarySearch/CapacityToShipPackages.java
@@ -3,39 +3,32 @@ package StriversAtoZDSACourse.BinarySearch;
 import java.util.Arrays;
 
 public class CapacityToShipPackages {
-    public static Boolean findDays(int[] arr, int limit, int mid){
+    public static int findDays(int[] arr, int limit, int mid) {
         int sum = 0;
         int days = 1;
-        Boolean areDaysInLimit = Boolean.FALSE;
-        for(int num : arr){
-            sum += num;
-            if(sum > mid){
+        for (int num : arr) {
+            if (sum + num > mid) {
                 days++;
+                sum = 0;
             }
-            if(days > limit){
-                return Boolean.FALSE;
-            }else{
-                areDaysInLimit = Boolean.TRUE;
-            }
+            sum += num;
         }
-        return areDaysInLimit;
+        return days;
     }
 
     public static int findTheMinimumCapacity(int[] arr, int days){
         int low = Arrays.stream(arr).max().getAsInt();
         int high = Arrays.stream(arr).sum();
-        int minCapacity = Integer.MAX_VALUE;
         while(low <= high){
             int mid = low + (high-low)/2;
-
-            if(!findDays(arr, days,mid)){
-                low = mid + 1;
-            }else{
-                minCapacity = Math.min(minCapacity, mid);
+            int daysToBeTaken = findDays(arr, days, mid);
+            if(daysToBeTaken <= days){
                 high = mid - 1;
+            }else{
+                low = mid + 1;
             }
         }
-        return minCapacity;
+        return low;
     }
 
     public static void main(String[] args) {
